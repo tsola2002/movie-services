8d3cb0d4f14af12883849d2fb041098b8278c61f
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-08-30 19:36:50 +0100

    5. injected an instance using @Bean annnotation

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,13 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+  @Autowired
+  private RestTemplate restTemplate;
+
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-    RestTemplate restTemplate = new RestTemplate();
+
 
     //Hardcode Rating API response
     List<Rating> ratings = Arrays.asList(
