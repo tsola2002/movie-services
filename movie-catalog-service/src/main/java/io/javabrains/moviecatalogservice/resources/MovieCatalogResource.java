c71af1780b507b842815549992a6763656e8ea5f
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-08-30 19:22:44 +0100

    4. making a call from one service to anoter

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,17 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,9 +19,21 @@ public class MovieCatalogResource {
 
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-    return Collections.singletonList(
-        new CatalogItem("Transformers", "Test", 4)
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    //Hardcode Rating API response
+    List<Rating> ratings = Arrays.asList(
+        new Rating("Top Gun", 4),
+        new Rating("Nemesis", 3)
     );
+
+    return ratings.stream().map(rating -> {
+          Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+          return new  CatalogItem(movie.getName(), "Desc", rating.getRating());
+        })
+        .collect(Collectors.toList());
+
   }
 
 }
