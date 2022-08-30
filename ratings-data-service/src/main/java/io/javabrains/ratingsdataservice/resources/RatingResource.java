fce0b0086d44f8cd60a99cdb7795c61bbb099e90
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-08-30 18:29:33 +0100

    3. created ratings data service endpoint

---
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingResource.java
@@ -0,0 +1,17 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+  @RequestMapping("/{movieId}")
+  public Rating getRating(@PathVariable("movieId") String movieId) {
+    return new Rating(movieId, 4);
+  }
+
+}
